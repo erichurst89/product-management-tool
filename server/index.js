dee4bdd79eb5b0d54526adbb7423f8e7f3295095
erichurst89 <157569916+erichurst89@users.noreply.github.com>
2024-01-27 00:29:07 -0600

    update

--- server/index.js
+++ server/index.js
@@ -29,17 +29,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
-/*const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello Eric');
-});
-
-app.listen(3001, () => {
-    console.log('Running on port 3001');
-})*/
