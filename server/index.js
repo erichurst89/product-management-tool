2e34eff86cac6a5b374c17fed4dc2945411cfe79
erichurst89 <157569916+erichurst89@users.noreply.github.com>
2024-01-26 21:37:19 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,40 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3001; // You can use any port number you prefer
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'inventoryprojectdb'
+});
+
+// Establish MySQL connection
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database: ', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Define routes
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is your backend server.');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+/*const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello Eric');
@@ -7,4 +42,4 @@ app.get('/', (req, res) => {
 
 app.listen(3001, () => {
     console.log('Running on port 3001');
-})
+})*/
